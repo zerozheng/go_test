ccf4128a184a75ed0560f02d45a2449d59cd55c5
后主 <houzhu.zheng@tuya.com>
2023-02-07 23:11:59 +0800

    feat：update

--- controllers/loginController.go
+++ controllers/loginController.go
@@ -1,28 +1,28 @@
 package controllers
 
-import (
-	"github.com/kataras/iris/mvc"
-	"github.com/kataras/iris/v12"
-)
+// import (
+// 	"github.com/kataras/iris/mvc"
+// 	"github.com/kataras/iris/v12"
+// )
 
-type LoginController struct {
-	Ctx iris.Context
-}
+// type LoginController struct {
+// 	Ctx iris.Context
+// }
 
-func (c *LoginController) BeforeActivation(b mvc.BeforeActivation) {
-	b.Handle("POST", "/signout", "LoginHandle")
+// func (c *LoginController) BeforeActivation(b mvc.BeforeActivation) {
+// 	b.Handle("POST", "/signout", "LoginHandle")
 
-	println("I am a BeforeActivation")
-}
+// 	println("I am a BeforeActivation")
+// }
 
-func (l *LoginController) LoginHandle() string {
-	println("I am a LoginHandle")
+// func (l *LoginController) LoginHandle() string {
+// 	println("I am a LoginHandle")
 
-	return "LoginHandle success"
-}
+// 	return "LoginHandle success"
+// }
 
-func (l *LoginController) Post() string {
-	println("I am a post")
+// func (l *LoginController) Post() string {
+// 	println("I am a post")
 
-	return "post success"
-}
+// 	return "post success"
+// }
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"test/controllers"
-
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/mvc"
 )
@@ -12,8 +10,30 @@ func main() {
 	app.Logger().SetLevel("debug")
 
 	mvc.Configure(app.Party("/api"), func(a *mvc.Application) {
-		a.Party("/login").Handle(new(controllers.LoginController))
+		a.Party("/login").Handle(new(LoginController))
 	})
 
 	app.Run(iris.Addr(":8080"))
+}
+
+type LoginController struct {
+	Ctx iris.Context
+}
+
+func (c *LoginController) BeforeActivation(b mvc.BeforeActivation) {
+	b.Handle("POST", "/signout", "LoginHandle")
+
+	println("I am a BeforeActivation")
+}
+
+func (l *LoginController) LoginHandle() string {
+	println("I am a LoginHandle")
+
+	return "LoginHandle success"
+}
+
+func (l *LoginController) Post() string {
+	println("I am a post")
+
+	return "post success"
 }
